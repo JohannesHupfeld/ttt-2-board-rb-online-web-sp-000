f8f2d9822a2bc87b2e5910a0f524d51c43a23a71
Johannes Hupfeld <j.hupfeld@gmail.com>
2020-03-19 16:23:16 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,19 @@
 # Define the variable board below.
+def board (row)
+    |   |
+______________
+    |   |
+______________
+    |   |
+
+end
+
+row = ["1","2","3","4","5","6","7","8","9"]
+
+row[0]
+
+row[1]
+
+row[2]
+
+row[1] ="X"
